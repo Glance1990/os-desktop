a1c1670e726c36584213acd16d593599f3f0fba6
Pavlo Kadysh <pavlo.kadysh@dataart.com>
2021-05-10 15:13:27 +0300

    fixing errors

--- helpers/createUpdateObjInfo.js
+++ helpers/createUpdateObjInfo.js
@@ -17,7 +17,8 @@ export const createUpdateObjInfo = (userInfo) => {
   console.log("userInfouserInfo", userInfo);
   const os = `${userInfo.os.type} ${userInfo.os.version} ${userInfo.os.arch}`;
   const webBrowser = `${userInfo.browser.name} ${userInfo.browser.version}`;
-  const resolution = `${userInfo.window.width} x ${userInfo.window.height}`;
+  const resolution = false;
+  //const resolution = `${userInfo.window.width} x ${userInfo.window.height}`;
 
   const customInfoObj = {
     os: {
