923cd83fd57842695d79a7022bb84f2dcea79cd6
Pavlo Kadysh <pavlo.kadysh@dataart.com>
2021-05-10 17:27:20 +0300

    add fixes

--- pages/index.js
+++ pages/index.js
@@ -85,10 +85,10 @@ export default function Home(props) {
   // fill summary data for questions from the server
   useEffect(() => {
     dispatch(
-      browserInfoActions.fillTilesDataAsync()
-      // browserInfoActions.fillTilesData(
-      //   props.currentPageReduxState.browserInfo.tilesInfo
-      // )
+      //browserInfoActions.fillTilesDataAsync()
+      browserInfoActions.fillTilesData(
+        props.currentPageReduxState.browserInfo.tilesInfo
+      )
     );
   }, []);
   //fill initial data that we got from the server
